9aa5a0bae97b393becdd169cfd45c1b07098989a
tom <t.wright.1@research.gla.ac.uk>
2023-11-22 09:57:37 +0000

    Added open source license and copyright

--- pep_genie/settings.py
+++ pep_genie/settings.py
@@ -1,3 +1,24 @@
+'''
+Copyright 2023, Thomas Wright
+
+LICENSE NOTICE
+This file is part of The Pep Genie.
+The Pep Genie is free software: 
+you can redistribute it and/or modify it under the terms of the 
+GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
+The Pep Genie is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with The Pep Genie.
+If not, see <https://www.gnu.org/licenses/>. 
+
+
+'''
+
+
+
 """
 Django settings for pep_genie project.
 
